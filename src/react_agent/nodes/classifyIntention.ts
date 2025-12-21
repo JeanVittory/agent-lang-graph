3d347345441081b27339c58410d40735f4a4cbc6
JeanVittory <vittory.dev@gmail.com>
2025-12-20 22:09:46 -0500

    Implement 2 new guardrails in classify intention node

--- src/react_agent/nodes/classifyIntention.ts
+++ src/react_agent/nodes/classifyIntention.ts
@@ -1,29 +1,42 @@
-import { RunnableConfig } from "@langchain/core/runnables";
-import { AgentState, ensureConfiguration } from "../configuration.js";
-import {  loadChatModel } from "../utils.js";
-import { CLASSIFIER_INTENT_PROMPT_AGENT_TEMPLATE } from "../prompts.js";
-import { IntentSchema } from "../schemas/classifyIntention.js";
+import { RunnableConfig } from '@langchain/core/runnables';
+import { AgentState, ensureConfiguration } from '../configuration.js';
+import { loadChatModel } from '../utils.js';
+import { CLASSIFIER_INTENT_PROMPT_AGENT_TEMPLATE } from '../prompts.js';
+import { IntentSchema } from '../schemas/classifyIntention.js';
+import { inputGuardrail } from '../guardrails/inputGuardrail.js';
+import { topicGuardrail } from '../guardrails/topicGuardrail.js';
 
 export async function classifyIntentionNode(
   state: typeof AgentState.State,
   config: RunnableConfig,
 ): Promise<typeof AgentState.Update> {
-  const configuration = ensureConfiguration(config)
-  const model = await loadChatModel(configuration.model)
-  const structuredModel = model.withStructuredOutput(IntentSchema)
-  const {intent} = await structuredModel.invoke([
+  const configuration = ensureConfiguration(config);
+  const gr = inputGuardrail(state.messages);
+  if (!gr.ok)
+    return {
+      intent: 'blocked',
+    };
+  const lastMessage = state.messages.at(-1)?.content.toString() || '';
+  const { inScope } = await topicGuardrail(lastMessage, 'gpt-4o-mini');
+  if (!inScope)
+    return {
+      intent: 'blocked',
+    };
+  const model = await loadChatModel(configuration.model);
+  const structuredModel = model.withStructuredOutput(IntentSchema);
+  const { intent } = await structuredModel.invoke([
     {
-      role: "system",
-      content: configuration.systemPromptTemplate
+      role: 'system',
+      content: configuration.systemPromptTemplate,
     },
     {
-      role: "system",
+      role: 'system',
       content: CLASSIFIER_INTENT_PROMPT_AGENT_TEMPLATE,
     },
     ...state.messages,
-  ])
+  ]);
 
   return {
     intent,
-  }
+  };
 }
