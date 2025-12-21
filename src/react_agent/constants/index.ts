5765d1420c20cf47c051423cd0a4ee9aabe6c374
JeanVittory <vittory.dev@gmail.com>
2025-12-21 00:16:45 -0500

    Add several models into a enum to easily pick them around the project

--- src/react_agent/constants/index.ts
+++ src/react_agent/constants/index.ts
@@ -1,1 +1,9 @@
 export const API_KEY_REGEX = /\bsk-(?:proj-|org-|live-)?[a-zA-Z0-9]{20,}\b/;
+
+export enum OPEN_AI_MODEL {
+  SIMPLE_FAST = 'gpt-4o-mini',
+  SIMPLE_ULTRA_CHEAP = 'gpt-4.1-nano',
+  SIMPLE_STRUCTURED = 'gpt-4.1-mini',
+  DECISION_AGENT = 'gpt-4.1',
+  CUSTOMER_SUPPORT = 'gpt-4o-mini',
+}
